f3d27fa7fafdd7a9a8489e39b060e623accdda85
Mark Boiko <markopopa555@gmail.com>
2022-08-06 00:23:32 +0300

    <<Third commit»

--- index.js
+++ index.js
@@ -1,85 +1,125 @@
+// import { checkresult } from 'module';
 const startGameEvent = new Event("gameStarted");
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const startGameButton = document.getElementById("startGame");
+//     startGameButton.onclick = () => {
+//         const mainMenu = document.querySelector(".mainMenu");
+//         const forms = document.getElementsByTagName("form");
+//         mainMenu.style.display = "none";
+//         forms.style.display = "flex";
+//     }
+// })
+
+const gameStates = {
+    xWon: 'xWon',
+    yWon: 'yWon',
+    draw: 'draw',
+}
+
+let gameResult;
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const button = document.getElementById("startGame");
+    const homeButton = document.querySelector(".home");
+    const resetButton = document.querySelector(".reset");
 
     button.onclick = () => {
-        const gameContainer = document.querySelector(".container");
+        const game = document.getElementById("game");
         const mainMenu = document.querySelector(".mainMenu");
-        gameContainer.style.display = "flex";
-        mainMenu.style.display = "none"
+        game.style.display = "block";
+        mainMenu.style.display = "none";
         document.dispatchEvent(startGameEvent);
+    }
+    homeButton.onclick = () => {
+        const game = document.getElementById("game");
+        const mainMenu = document.querySelector(".mainMenu");
+        game.style.display = "none";
+        mainMenu.style.display = "flex";
+    }
+    resetButton.onclick = () => {
+
     }
 
     document.addEventListener('gameStarted', () => {
-        let listOfTicTacToe = Array(9).fill(0).map((_, index) => { return { id: index, value: null, bool: true } });
+        let listOfTicTacToe = Array(9).fill(0).map((_, index) => { return { id: index, value: null, isFilled: false } });
         const board = document.querySelectorAll(".block");
         let flag = true;
-        let sum = 0;
         function checkresult(list) {
             for (let i = 0; i < 9; i += 3) {
-                if(list[i].value != null && (list[i].value == list[i+1].value && list[i].value == list[i+2].value)){
+                if (list[i].value != null && (list[i].value == list[i + 1].value && list[i].value == list[i + 2].value)) {
                     return true;
                 }
             }
-            for (let i = 0; i < 3; i ++) {
-                if(list[i].value != null && (list[i].value == list[i+3].value && list[i].value == list[i+6].value)){
+            for (let i = 0; i < 3; i++) {
+                if (list[i].value != null && (list[i].value == list[i + 3].value && list[i].value == list[i + 6].value)) {
                     return true;
                 }
             }
-            for (let i = 0; i < 1; i ++) {
-                if(list[i].value != null && (list[i].value == list[i+4].value && list[i].value == list[i+8].value)){
+            for (let i = 0; i < 1; i++) {
+                if (list[i].value != null && (list[i].value == list[i + 4].value && list[i].value == list[i + 8].value)) {
                     return true;
                 }
             }
-            for (let i = 2; i > 1 ; i--) {
-                if(list[i].value != null && (list[i].value == list[i+2].value && list[i].value == list[i+4].value)){
+            for (let i = 2; i > 1; i--) {
+                if (list[i].value != null && (list[i].value == list[i + 2].value && list[i].value == list[i + 4].value)) {
                     return true;
                 }
             }
             return false;
 
-            
-            
+
+
         }
         board.forEach((cell) => {
             cell.onclick = () => {
-                listOfTicTacToe.forEach((object, index) => {
 
+                listOfTicTacToe.forEach((object, index) => {
                     if (cell.id == object.id) {
-                        if (object.bool === true) {
+                        if (object.isFilled === false) {
                             if (flag === true) {
                                 listOfTicTacToe[index].value = 1;
                                 cell.innerHTML = '<img src="Assets/x.png">';
                                 flag = false;
-                                object.bool = false;
+                                object.isFilled = true;
                                 let winerX = checkresult(listOfTicTacToe);
-                                if(winerX == true){
+                                if (winerX == true) {
                                     console.log("Win X");
+                                    gameResult = gameStates.xWon;
+                                    return;
                                 }
-                                else{
-                                    console.log("Draw")
-                                }
+
                             }
                             else {
                                 listOfTicTacToe[index].value = 2;
                                 cell.innerHTML = '<img src="Assets/circle.png">';
                                 flag = true;
-                                object.bool = false;
+                                object.isFilled = true;
                                 let winerO = checkresult(listOfTicTacToe);
-                                if(winerO == true){
+                                if (winerO == true) {
                                     console.log("Win O");
+                                    gameResult = gameStates.yWon;
+                                    return;
                                 }
-                                else{
-                                    console.log("Draw")
-                                }
+
                             }
                         }
 
                     }
 
                 })
-                console.log(listOfTicTacToe);
+
+                const isDraw = listOfTicTacToe.every((cell) => {
+                    return cell.isFilled === true;
+                });
+
+
+                if (isDraw) {
+                    console.log('draw!!!')
+                    gameResult = gameStates.draw;
+                }
+
+
+
             }
         });
 
